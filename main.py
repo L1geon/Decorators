7f5c5709cdfd71f0c18a7408fd0f49b148bc8122
L1geon <ligeon1@piterov.ru>
2022-06-29 01:31:44 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from tools import loger
+import requests
+
+
+@loger(log_path="logs\logs.log")
+def get_weather(city: str):
+    api = "a9e7fe609b9eb44ad04ff559c707b0e3"
+    params = {"q": city, "type": "like", "units": "metric", "lang": "ru", "APPID": api}
+    r = requests.get("http://api.openweathermap.org/data/2.5/find", params=params)
+    data = r.json()
+    print(f"""        id города в системе OpenWeatherMap: {data["list"][0]["id"]}
+        Координаты города: {data["list"][0]["coord"]}
+        Текущая температура: {data["list"][0]["main"]["temp"]}°C
+        Облака: {data["list"][0]["weather"][0]["description"]}""")
+    return data
+
+
+if __name__ == "__main__":
+    get_weather("Nizhny Novgorod, RU")
---
+++ tools.py
@@ -0,0 +1,18 @@
+from functools import wraps
+import datetime
+
+
+def loger(log_path):
+    def _loger(old_function):
+        @wraps(old_function)
+        def new_function(*args, **kwargs):
+            log = {}
+            Key = f"{args}{kwargs}"
+            result = old_function(*args, **kwargs)
+            log[Key] = f"{result} time-{datetime.datetime.now()}"
+            with open(log_path, "a", encoding="utf-8") as f:
+                for key, val in log.items():
+                    f.write("{}:{}\n".format(key, val))
+            return new_function
+        return new_function
+    return _loger
